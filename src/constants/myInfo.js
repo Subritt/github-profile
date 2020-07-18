19a37c8b338c2882e48c1365f71884a49527ea7f
Subritt <subrittburlakoti@gmail.com>
2020-07-18 11:49:43 +0545

    changed details and profile picture

--- src/constants/myInfo.js
+++ src/constants/myInfo.js
@@ -1,21 +1,21 @@
 export const myGithub = 'aakritsubedi';
 
 export const myInfo = {
-  firstName: 'Aakrit',
-  lastName: 'Subedi',
-  address: 'Basundhara, Kathmandu',
-  email: 'aakritsubedi9@gmail.com'
+  firstName: 'Subritt',
+  lastName: 'Burlakoti',
+  address: 'Budhanilkantha, Kathmandu',
+  email: 'subrittburlakoti@gmail.com'
 }
 
-export const whoAmI = [' Computer Engineer.', ' Web Developer.', ' AI Enthusiast.']
+export const whoAmI = [' Computer Engineer.', ' casual Developer.', ' Data Analyst.']
 
-export const techStack = ['HTML', 'CSS', 'JavaScript', 'node', 'PHP', 'Laravel', 'Java', 'C', 'C++', 'SQL', 'Git']
+export const techStack = ['HTML', 'CSS', 'JavaScript', 'Java', 'C', 'C++', 'SQL', 'Git', 'Google Script', 'Google Sheets', 'Google Data']
 
 export const socialMedia = [
-  { title: 'facebook', link: 'https://www.facebook.com/aakritsubedi9'},
-  { title: 'instagram', link: 'https://instagram.com/aakrit_subedi'},
-  { title: 'twitter', link: 'https://twitter.com/SubediAakrit'},
-  { title: 'github', link: 'https://github.com/aakritsubedi'},
-  { title: 'linkedIn', line: 'https://www.linkedin.com/in/aakrit-subedi-942a5875/'},
-  { title: 'youtube', link: 'http://youtube.com/channel/UCQa_5vTnUERh2qAGiObqizQ'}
+  // { title: 'facebook', link: 'https://www.facebook.com/aakritsubedi9'},
+  // { title: 'instagram', link: 'https://instagram.com/aakrit_subedi'},
+  { title: 'twitter', link: 'https://twitter.com/SubrittB'},
+  { title: 'github', link: 'https://github.com/subritt'},
+  { title: 'linkedIn', line: 'https://www.linkedin.com/in/subritt-burlakoti-537605159/'},
+  // { title: 'youtube', link: 'http://youtube.com/channel/UCQa_5vTnUERh2qAGiObqizQ'}
 ]
